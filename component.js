f6374f43f6fc57fc9c42685bbf5b4e68f3d1df6f
Mwarukason <a.mwaruka93@gmail.com>
2017-03-15 15:52:25 +0800

    changes in css n js

--- component.js
+++ component.js
@@ -2,65 +2,66 @@ var destination = document.querySelector("#app");
 
     var TodoItems = React.createClass({
       render: function() {
-        var todoEntry = this.props.entries;
+        var todoEntries = this.props.entries;
 
         function createTasks(item) {
           return <li key={item.key}>{item.text}</li>
         }
 
-        var listElement = todoEntry.map(createTasks);
+        var listItems = todoEntries.map(createTasks);
 
         return (
-          <ul className="elemList">
-            {listElement}
+          <ul className="theList">
+            {listItems}
           </ul>
         );
       }
     });
+
     var TodoList = React.createClass({
-          getInitialState: function() {
-            return {
-              items: []
-            };
-          },
-          addItem: function(e) {
-            var itemArray = this.state.items;
+      getInitialState: function() {
+        return {
+          items: []
+        };
+      },
+      addItem: function(e) {
+        var itemArray = this.state.items;
 
-            itemArray.push(
-              {
-                text: this._inputElement.value,
-                key: Date.now()
-              }
-            );
-
-            this.setState({
-              items: itemArray
-            });
-
-            this._inputElement.value = "";
-
-            e.preventDefault();
-          },
-          render: function() {
-            return (
-              <div className="todo_class">
-                <div className="todo_subclass">
-                  <form onSubmit={this.addItem}>
-                    <input ref={(a) => this._inputElement = a}
-                           placeholder="Enter Your Task">
-                    </input>
-                    <button type="submit">ADD</button>
-                  </form>
-                </div>
-                <TodoItems entries={this.state.items}/>
-              </div>
-            );
+        itemArray.push(
+          {
+            text: this._inputElement.value,
+            key: Date.now()
           }
+        );
+
+        this.setState({
+          items: itemArray
         });
 
-        ReactDOM.render(
-          <div>
-            <TodoList/>
-          </div>,
-          destination
+        this._inputElement.value = "";
+
+        e.preventDefault();
+      },
+      render: function() {
+        return (
+          <div className="todoListMain">
+            <div className="header">
+              <form onSubmit={this.addItem}>
+                <input align="middle" ref={(a) => this._inputElement = a}
+                       placeholder="Enter Your Tasks">
+                </input>
+                <button type="submit">ADD</button>
+              </form>
+            </div>
+            <TodoItems entries={this.state.items}/>
+          </div>
         );
+      }
+    });
+
+    ReactDOM.render(
+      <div>
+        <TodoList/>
+      </div>,
+      destination
+    );
